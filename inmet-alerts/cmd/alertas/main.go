cb7d788a2a275885277dfdfd36044688b12b7e51
jeferson <jefersonnpn@gmail.com>
2023-12-04 19:52:36 -0300

    :lipstick: logging inmet alerts info

--- inmet-alerts/cmd/alertas/main.go
+++ inmet-alerts/cmd/alertas/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/jefersonf/mini-go-projects/inmet-alerts/pkg/inmet"
 )
 
 func main() {
-	inmet.FetchData()
+	activeAlerts, err := inmet.FetchData()
+	if err != nil {
+		panic(err)
+	}
+
+	today, future := activeAlerts.GetAll()
+	log.Printf("Active Weather Alerts: %d, today (%d) future (%d)\n", activeAlerts.Count(), today.Count(), future.Count())
 }
