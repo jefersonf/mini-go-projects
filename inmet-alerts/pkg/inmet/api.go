5ab0bb5689b6aebe813d9aacd83a6cced7d4ad66
jeferson <jefersonnpn@gmail.com>
2023-12-05 11:13:07 -0300

    :arrow_up:  error messages

--- inmet-alerts/pkg/inmet/api.go
+++ inmet-alerts/pkg/inmet/api.go
@@ -2,14 +2,18 @@ package inmet
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"log"
 	"net/http"
 )
 
-const (
-	ActiveAlertsEndpoint = "https://apiprevmet3.inmet.gov.br/avisos/ativos"
-	// alertEndpoint = "https://apiprevmet3.inmet.gov.br/aviso/getByID/%v"
+const ActiveAlertsEndpoint = "https://apiprevmet3.inmet.gov.br/avisos/ativos"
+
+var (
+	ErrUnableToFetchDataFromINMET = errors.New("unable to get data from INMET")
+	ErrUnableToReadINMETData      = errors.New("unable to read INMET data")
+	ErrUnableToParseINMETData     = errors.New("unable to parse INMET data")
 )
 
 func FetchData() (*ActiveAlerts, error) {
@@ -19,18 +23,18 @@ func FetchData() (*ActiveAlerts, error) {
 	log.Println("Fetching data from INMET..")
 	resp, err := http.Get(ActiveAlertsEndpoint)
 	if err != nil {
-		return &alerts, err
+		return &alerts, ErrUnableToFetchDataFromINMET
 	}
 	defer resp.Body.Close()
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return &alerts, err
+		return &alerts, ErrUnableToReadINMETData
 	}
 
 	err = json.Unmarshal(data, &alerts)
 	if err != nil {
-		return &alerts, err
+		return &alerts, ErrUnableToParseINMETData
 	}
 
 	return &alerts, nil
