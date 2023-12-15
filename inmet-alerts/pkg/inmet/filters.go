8c19bcb8bfedd6956d6c3b0ee09fc3c6be28adc9
jefersonf <jefersonnpn@gmail.com>
2023-12-15 00:36:43 -0300

    add severity filter

--- inmet-alerts/pkg/inmet/filters.go
+++ inmet-alerts/pkg/inmet/filters.go
@@ -13,3 +13,13 @@ func (wa *weatherAlerts) ByRegion(region string) weatherAlerts {
 	}
 	return alerts
 }
+
+func (wa *weatherAlerts) BySeverity(severity string) weatherAlerts {
+	alerts := make(weatherAlerts, 0)
+	for _, alert := range *wa {
+		if strings.Compare(alert.Severity, severity) == 0 {
+			alerts = append(alerts, alert)
+		}
+	}
+	return alerts
+}
