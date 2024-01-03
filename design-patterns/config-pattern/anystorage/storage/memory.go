f198f929f0ab386d8a00fa686be29c4dfe7a9b77
Jeferson Ferreira <jefersonnpn@gmail.com>
2024-01-02 23:42:25 -0300

    Docs

--- design-patterns/config-pattern/anystorage/storage/memory.go
+++ design-patterns/config-pattern/anystorage/storage/memory.go
@@ -1,27 +1,32 @@
 package storage
 
+// MemoryRepository provides access to memory storage.
 type MemoryRepository struct {
 	data []Data
 }
 
+// WithMemoryRepository is a storage option that enables storage in memory.
 func WithMemoryRepository() Option {
 	return func(o *StorageOption) error {
 		return nil
 	}
 }
 
+// init function initialize the in-memory repository.
 func (s *MemoryRepository) init() {
 	if s.data == nil {
 		s.data = make([]Data, 0)
 	}
 }
 
+// Add persists some data in memory.
 func (s *MemoryRepository) Add(d Data) error {
 	s.init()
 	s.data = append(s.data, d)
 	return nil
 }
 
+// GetAll returns all data saved on storage.
 func (s *MemoryRepository) GetAll() []Data {
 	s.init()
 	return s.data
