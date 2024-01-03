5b884cbd4728b8cfb7c4f1d1f9eedb2a25f5d9da
Jeferson Ferreira <jefersonnpn@gmail.com>
2024-01-02 23:36:26 -0300

    Add comments to option and storage option types

--- design-patterns/config-pattern/anystorage/storage/options.go
+++ design-patterns/config-pattern/anystorage/storage/options.go
@@ -1,7 +1,9 @@
 package storage
 
+// Option is a function that represents a storage option setter.
 type Option func(*StorageOption) error
 
+// StorageOption represents the storage options.
 type StorageOption struct {
 	MemoryDriver *MemoryRepository
 	MongoDriver  *MongoRepository
