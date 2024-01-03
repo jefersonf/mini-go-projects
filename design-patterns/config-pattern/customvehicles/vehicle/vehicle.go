b958992ff6cb9a680c17960aedf6a06b226f7846
Jeferson Ferreira <jefersonnpn@gmail.com>
2024-01-03 00:13:58 -0300

    Update vehicle.go

--- design-patterns/config-pattern/customvehicles/vehicle/vehicle.go
+++ design-patterns/config-pattern/customvehicles/vehicle/vehicle.go
@@ -2,11 +2,13 @@ package vehicle
 
 import "log"
 
+// Vehicle represets an vehicle that could have costum options.
 type Vehicle struct {
 	VehicleOptions
 }
 
-func NewVehicle(opts ...Option) *Vehicle {
+// New creates a new vehicle that can be customized by input opti meons.
+func New(opts ...Option) *Vehicle {
 	options := defaultOptions()
 	for _, customOption := range opts {
 		err := customOption(&options)
